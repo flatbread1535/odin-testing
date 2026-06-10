de06d9046703fe895583fe700ce626c02f735988
Adam <ajlarson0731@gmail.com>
2026-06-10 03:40:57 -0400

    Project setup for linters, formatters, installing Jest and other necessary packages, and creating src and test files.

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+import { defineConfig } from "eslint/config";
+import js from "@eslint/js";
+import jest from "eslint-plugin-jest";
+
+export default defineConfig([
+	{
+		files: ["**/*.js"],
+		plugins: {
+			js,
+			jest,
+		},
+		extends: ["js/recommended", "jest/recommended"],
+		rules: {
+			"no-unused-vars": "warn",
+			"no-undef": "warn",
+		},
+		languageOptions: {
+			globals: {
+				describe: "readonly",
+				it: "readonly",
+				test: "readonly",
+				expect: "readonly",
+				beforeEach: "readonly",
+				afterEach: "readonly",
+				beforeAll: "readonly",
+				afterAll: "readonly",
+			},
+    },
+  },
+]);
